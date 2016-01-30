25b78e06cfba2b24d3d65fb86d20ecdbb3e71762
Justin Barber <brbr@outlook.com>
2016-01-30 01:32:47 -0500

    Removing silliness

--- app/renderer/index.js
+++ app/renderer/index.js
@@ -4,10 +4,7 @@ let Archive = requireLocal('app/models/archive.js');
 
 module.exports = function(req, res)
 {   'use strict';
-    console.log('Got it baby');
     Archive.find({}, '-__v', (err, results) => {
-        console.log('oh yeah');
-        console.log(results);
         if(err) console.error(err);
 
         else {
