87e5b543ae37eb2259976e9d83f4ebfe912f84db
Justin Barber <brbr@outlook.com>
2016-01-30 22:09:07 -0500

    Archive audio and web pages
    Using phantom js, which must be installed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('./require-helpers.js');
 global.baseDir = __dirname;
 console.log(global.baseDir);
 
-let config = requireConfig('database.js');
+let databaseConfig = requireConfig('database.js');
 
 let express = require('express');
 let app = express();
@@ -12,14 +12,14 @@ let app = express();
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 
-console.log('Connecting to db on', config.url);
-mongoose.connect(config.url);
+console.log('Connecting to db on', databaseConfig.url);
+mongoose.connect(databaseConfig.url);
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
 
 requireLocal('app/routes.js')(app);
 
--- public/windham.js
+++ public/windham.js
@@ -1,6 +1,6 @@
-'use strict';
-
-angular.module('windham', ['ngResource'])
+(function(window)
+{   'use strict';
+    angular.module('windham', ['ngResource'])
         .factory('Archive', ['$resource', ($resource) => {
             return $resource('/api/archive', null, {
                 update: { method: 'PUT' },
@@ -8,6 +8,7 @@ angular.module('windham', ['ngResource'])
         }])
         .controller('ArchiveInputController', ['$scope', 'Archive', function($scope, Archive) {
             $scope.master = {};
+            $scope.archives = [];
 
             $scope.save = (input) => {
                 let archive = new Archive(input);
@@ -15,6 +16,56 @@ angular.module('windham', ['ngResource'])
                 archive.$save(() => {
                     $scope.archives.push(archive);
                     $scope.input = {};
+                    document.getElementById('audio-input-label').value = '';
                 })
-            };
-        }]);
+            }
+        }])
+        .controller('TabsController', ['$scope', function($scope) {
+            $scope.tabs = $('.tab');
+
+            $scope.displayTab = ($event) => {
+                let tabToShow = $($event.target.parentElement);
+                let contentToShow = $('#' + tabToShow.data('content'));
+
+                for(let t = 0; t < $scope.tabs.length; t++)
+                {
+                    let tab = $($scope.tabs[t]);
+                    let tabContent = $('#' + $(tab).data('content'));
+
+                    if(tab.hasClass('active'))
+                    {
+                        tab.removeClass('active');
+                        tabContent.removeClass('tab-content-active');
+                        tabContent.addClass('tab-content-inactive');
+
+                        tabToShow.addClass('active');
+                        contentToShow.addClass('tab-content-active');
+                        contentToShow.removeClass('tab-content-inactive');
+
+                        return;
+                    }
+                }
+            }
+        }])
+        .directive('fileread', [function() {
+            return {
+                scope: {
+                    fileread: '=',
+                },
+
+                link: function(scope, element, attributes) {
+                    element.bind('change', function(changeEvent) {
+                        let reader = new FileReader();
+                        reader.onload = function(loadEvent) {
+                            scope.$apply(function() {
+                                scope.fileread = loadEvent.target.result;
+                            });
+                        }
+                        reader.readAsDataURL(changeEvent.target.files[0]);
+
+                        document.getElementById('audio-input-label').value = changeEvent.target.files[0].name;
+                    });
+                }
+            }
+        }])
+})(window);
