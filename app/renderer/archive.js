cb56747441aac7fe83e08117e7ed9c8a29a17622
Justin Barber <brbr@outlook.com>
2016-01-30 23:54:51 -0500

    Archive Page
    You can now view an individual archive on its own page. The page also
    has a delete button to delete it.
    
    I’ve also optimized some of the EJS templates by removing some of the
    redundant code. It’s no faster probably but it’s more maintainable.

---
+++ app/renderer/archive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let Archive = requireLocal('app/models/archive.js');
+
+module.exports = function(req, res)
+{   'use strict';
+    Archive.findOne({ _id: req.params.id }, '-__v', (err, archive) => {
+        console.log(archive);
+        if(err || !archive)
+            res.render('pages/error', {message: 'Tha archive doesn\'t exist! Find one that does <a href="/">here.</a>'});
+        else {
+            res.render('pages/archive', {
+                archive: archive
+            });
+        }
+    });
+}
